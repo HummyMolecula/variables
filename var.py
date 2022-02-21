a56c7a896029b104183fb708336791edbbe503e3
HummyMolecula <dragonitdeveloper@gmail.com>
2022-02-21 16:30:51 +0200

    variables simple update

--- var.py
+++ var.py
@@ -1,5 +1,6 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
+num1 += 5
 
 print("Result:", num1 + num2)
 print("Result:", num1 - num2)
@@ -7,3 +8,8 @@ print("Result:", num1 / num2)
 print("Result:", num1 * num2)
 print("Result:", num1 ** num2)
 print("Result:", num1 // num2)
+print("Result:", num1 % num2)
+
+word = input("Enter word: ")
+print("Word(bool):", bool(word))
+print("Word * 2:", word * 2)
